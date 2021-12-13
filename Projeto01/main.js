0f3485856c4ff1cbab0c85b85e5d2ad8485a05f9
fabbioSborges <fabbioanderson@gmail.com>
2021-12-13 17:44:01 -0300

    Aula 05

---
+++ Projeto01/main.js
@@ -0,0 +1,37 @@
+const nav = document.querySelector("#header nav");
+const chave = document.querySelectorAll("#header .chave");
+
+for (const element of chave) {
+  element.addEventListener("click", function () {
+    nav.classList.toggle("show");
+  });
+}
+
+const links = document.querySelectorAll("nav ul li a");
+for (const link of links) {
+  link.addEventListener("click", function () {
+    nav.classList.remove("show");
+  });
+}
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
